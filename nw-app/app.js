a5596e9bdd6927a9a46487558bd9447b83f6de89
Brannon Dorsey <brannon@brannondorsey.com>
2016-08-13 13:06:19 -0500

    trace error event added to nw-app and cleanup

--- nw-app/app.js
+++ nw-app/app.js
@@ -5,27 +5,27 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-
 app.get('/traceroute', (req, res) => {
-	if(typeof req.query.location !== "undefined"){
+	if(typeof req.query.location !== 'undefined'){
 		res.send('location received'); // sending back confirmation to client, not working for some reason
 		geoTracer.trace(req.query.location);
 	}
 });
 
 io.on('connection', function(socket){
-	console.log('a client connected');
+	console.log('[nw-app] A client connected.');
 	socket.on('disconnect', function(){
-		console.log('client disconnected');
+		console.log('[nw-app] A client disconnected.');
+	});
+
+	geoTracer.on('error', err => { 
+		socket.emit('trace error', err);
 	});
 });
 
+http.listen(3001, () => console.log('[nw-app] Server listening on http://localhost:3001'));
 
-http.listen(3001, () => {
-	console.log('Server listening on http://localhost:3001');
-});
-
-geoTracer.on('trace-started', destination => console.log(`Trace started to ${destination}`));
+geoTracer.on('trace-started', destination => console.log(`[nw-app] Trace started to ${destination}`));
 geoTracer.on('ordered-hop', hop => {
 	let str;
 	if (hop.geo) {
@@ -34,22 +34,18 @@ geoTracer.on('ordered-hop', hop => {
 	} else {
 		str = `hop #${hop.hop}: ${hop.ip}`
 	}
-	console.log(str)
+	
+	console.log(`[nw-app] ${str}`);
 
 	// let addon worker know of hop
 	io.emit('trace hop', hop ); 
 });
 
 geoTracer.on('trace-finished', hops => {
-	console.log('Trace complete');
+	console.log('[nw-app] Trace complete');
 
 	// let addon worker know of trace completion
 	io.emit('trace complete', hops ); 
 });
-
-geoTracer.on('error', err => { 
-	throw err;
-});
-
 
 
