b4f9eb73e435f68640acd548aa9184d7c7e33982
Brannon Dorsey <brannon@brannondorsey.com>
2016-08-26 11:25:25 -0500

    InfrastructureAugmenter is now working

--- nw-app/InfrastructureAugmenter.js
+++ nw-app/InfrastructureAugmenter.js
@@ -10,6 +10,9 @@ class InfrastructureAugmenter {
 		this.aggregatedData = null;
 		this.buildingsGeo = null;
 		this.landingsGeo = null;
+		this.landingsGeoById = null;
+		this.cablesGeo = null;
+		this.cablesGeoById = null;
 		this.oceanGeo = null;
 		this.loaded = false;
 		
@@ -25,12 +28,28 @@ class InfrastructureAugmenter {
 					fs.readFile('maps/ocean.json', 'utf8', (err, data) => {
 						if (err) { callback(err); return; }
 						this.oceanGeo = JSON.parse(data);
-						this.loaded = true;
-						callback(null);
+							fs.readFile('maps/cable-data.json', 'utf8', (err, data) => {
+							if (err) { callback(err); return; }
+							this.cablesGeo = JSON.parse(data);
+							this.loaded = true;
+							this.landingsGeoById = this._generateGeoById(this.landingsGeo, 'id');
+							this.cablesGeoById = this._generateGeoById(this.cablesGeo, 'cable_id');
+							//console.log(this.cablesGeoById[Object.keys(this.cablesGeoById)[0]])
+							callback(null);
+						});
 					});
 				});
 			});
 		});
+	}
+
+	_generateGeoById(geoObj, propName) {
+		let geoById = {};
+		geoObj.features.forEach(feature => {
+			let prop = feature.properties[propName];
+			geoById[prop] = feature; // DANGER DANGER: 
+		});
+		return geoById;
 	}
 
 	addInfrastructureData(hop, nextHop) {
@@ -38,8 +57,8 @@ class InfrastructureAugmenter {
 		var self = this; // gross
 		hop.infrastructure = {
 			exchanges: [],
-			cables: [],
-			landings: []
+			landings: [],
+			cable: null
 		};
 
 		if (hop.geo && this.loaded) {
@@ -89,68 +108,78 @@ class InfrastructureAugmenter {
 						  }
 						};
 
-						let nearHop = [];
-						let nearNextHop = [];
-						let radius = 1500;
-						// Note: may need to not find nearest n landing points instead of look inside radius
-						// to account for instances where no landing points are within radius
-						let dists = [];
-
+						let landingNearHop = [];
+						let landingNearNextHop = [];
+						
 						this.landingsGeo.features.forEach((feature, i) => {
 							
-							let dist = turf.distance(hopGeoJSON, feature, 'kilometers');
-							if (dist <= radius) nearHop.push({ dist, 
-								                              coords: feature.geometry.coordinates, 
-								                              id: feature.properties.id,
-								                              cableId: feature.properties.cable_id/*getCableIds(feature.properties.cables)*/});
+							//console.log(feature);
+							//return;
+							landingNearHop.push({ dist: turf.distance(hopGeoJSON, feature, 'kilometers'), 
+										   feature: feature,
+			                               coords: feature.geometry.coordinates, 
+			                               id: feature.properties.id,
+			                               cableId: feature.properties.cable_id});
 							
-							dist = turf.distance(nextHopGeoJSON, feature, 'kilometers');
-							if (dist <= radius) {
-								nearNextHop.push({ dist, 
-					                               coords: feature.geometry.coordinates, 
-					                               id: feature.properties.id,
-					                               cableId: feature.properties.cable_id/*getCableIds(feature.properties.cables)*/});
-							}
+							landingNearNextHop.push({ dist: turf.distance(nextHopGeoJSON, feature, 'kilometers'), 
+				                               feature: feature,
+				                               coords: feature.geometry.coordinates, 
+				                               id: feature.properties.id,
+				                               cableId: feature.properties.cable_id});
+							
 						});
 
-						console.log("Landing near this hop: " + nearHop.length);
-						console.log("Landing near next hop: " + nearNextHop.length);
-						console.log('STARTED CABLE LOOKUP');
-						let cables = getCables();
-						console.log('FINISHED CABLE LOOKUP');
-						cables.forEach(cable => {						
-							if (cable) {
-								console.log(`FOUND A CABLE: ${cable.name}`);
-								console.log(cable);
-							} else {
-								console.log('CABLE NOT FOUND');
-							}
-						});
+						landingNearHop = _.sortBy(landingNearHop, function(hop) { return hop.dist });
+						landingNearNextHop = _.sortBy(landingNearNextHop,function(hop) { return hop.dist });					
+						
+						let c = getCables()[0];
+						
+						hop.infrastructure.landings.push(c.start);
+						hop.infrastructure.landings.push(c.end);
+						hop.infrastructure.cable = c.cable;
 
+						console.log(`${c.cable.properties.name} START: ${c.distStart} END: ${c.distEnd} SUM: ${c.distSum}`);
+
+						// cables.forEach(c => {						
+						// 	if (c) {
+						// 		console.log(`${c.cable.properties.name} START: ${c.distStart} END: ${c.distEnd} SUM: ${c.distSum}`);
+						// 		hop.infrastructure.landings.push(c.start);
+						// 		hop.infrastructure.landings.push(c.end);
+						// 		hop.infrastructure.cable = c.cable;
+						// 	} else {
+						// 		console.log('CABLE NOT FOUND');
+						// 	}
+						// });
 
 						function getCables() {
+
 							let cables = [];
-							// For all landing points near the hop
-							for (let i = 0; i < nearHop.length; i++) {
-								let cableId = nearHop[i].cableId;
-								// validate this is a cable we have in the dict
-								//Object.keys(self.aggregatedData).forEach(key => console.log('key' + key))
-								if (self.aggregatedData.cable[cableId.toString()]) {
-									
-									for (let k = 0; k < self.aggregatedData.cable[cableId].landing_points.length; k++) {
-										let landing = self.aggregatedData.cable[cableId].landing_points[k];
-										// For all landing points near the next hop
-										for (let l = 0; l < nearNextHop.length; l++) {
-											//console.log(nearNextHop[l].id, landing.id)
-											if (nearNextHop[l].id == landing.id) {
-												cables.push(self.aggregatedData.cable[cableId]);
-												//return self.aggregatedData.cable[cableId];
-											}
+							// For each landing points near the hop
+							for (let i = 0; i < landingNearHop.length; i++) {
+								// get that landing point's id
+								let cableId = landingNearHop[i].feature.properties.cable_id;
+								// For each landing point that cable has								
+								for (let k = 0; k < self.aggregatedData.cable[cableId].landing_points.length; k++) {
+									let landing = self.aggregatedData.cable[cableId].landing_points[k];
+									// For all landing points near the next hop
+									for (let l = 0; l < landingNearNextHop.length; l++) {
+										if (landingNearNextHop[l].feature.properties.id == landing.id && 
+											landingNearNextHop[l].feature.properties.id != landingNearHop[i].feature.properties.id) {	
+											cables.push({
+												start: landingNearHop[i].feature,
+												end: landingNearNextHop[l].feature,
+												cable: self.cablesGeoById[cableId],
+												distSum: landingNearHop[i].dist + landingNearNextHop[l].dist,
+												distStart: landingNearHop[i].dist,
+												distEnd: landingNearNextHop[l].dist 
+											});
+
 										}
-									} 
+									}
 								}							
 							}
-							return cables;
+
+							return _.uniq(_.sortBy(cables, cable => cable.distSum), cable => cable.cable.properties.id);
 						}
 
 						function getCableIds(cables) {
@@ -158,45 +187,16 @@ class InfrastructureAugmenter {
 							cables.forEach(({cable_id}) => ids.push(parseInt(cable_id)));
 							return ids;
 						}
-
-						// if (nearby.length > 0) {
-						// 	if (nearby.length > 1) nearby = _.sortBy(nearby, obj => obj.dist);
-						// 	hop.infrastructure.exchanges.push(nearby[0]);
-						// 	// for each nearby landing point...
-						// 	nearby.forEach(obj => {
-						// 		// look up their cables, and see if they the other end is near
-						// 		// the next hop
-						// 		let cable = this.aggregatedData.cables[obj.cableId];
-						// 		if (cable) {
-						// 			let land = null;
-						// 			cable.landing_points.forEach(landing => {
-						// 				if (landing.id != obj.id) {
-						// 					this.aggregatedData.cables[obj.cableId]
-						// 				} 
-						// 			});
-						// 		}
-						// 	});
-						// } 
 					}
 				}
-
-				
-
-			} else { // this is the next hop
-
 			}
-
 		}
-	}
-
-	_complexify(points) {
-		return complexify(points, 200);
 	}
 
 	_crossesOcean(points) {
 		
 		let inside = false;
-		let numPointsMustBeInOcean = 3;
+		let numPointsMustBeInOcean = 2;
 		let numPointsInOcean = 0;
 		
 		points = complexify(points, 150);
@@ -220,6 +220,7 @@ class InfrastructureAugmenter {
 		
 		return inside;
 	}
+
 }
 
 module.exports = InfrastructureAugmenter;
