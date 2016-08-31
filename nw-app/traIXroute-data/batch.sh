bfce663bc02f7ed50bcaf2d8d1f57ee63f608036
Brannon Dorsey <brannon@brannondorsey.com>
2016-08-30 21:43:30 -0500

    Add nw-app/traIXroute-data

---
+++ nw-app/traIXroute-data/batch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in `seq 1 50`;
+do
+	node test-ip-prefixes.js
+done    
---
+++ nw-app/traIXroute-data/node_modules/nodejs-traceroute/example.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Traceroute = require('./index');
+
+try {
+    const tracer = new Traceroute();
+    tracer
+        .on('pid', (pid) => {
+            console.log(`pid: ${pid}`);
+        })
+        .on('destination', (destination) => {
+            console.log(`destination: ${destination}`);
+        })
+        .on('hop', (hop) => {
+            console.log(`hop: ${JSON.stringify(hop)}`);
+        })
+        .on('close', (code) => {
+            console.log(`close: code ${code}`);
+        });
+
+    tracer.trace('github.com');
+} catch (ex) {
+    console.log(ex);
+}
